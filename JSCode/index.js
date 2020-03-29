b18a33368d8d7d10ed8b57723442ed330b4b2404
Simon Ribaut <simon.ribaut@outlook.com>
2020-03-29 22:37:22 +0200

    ajout de la liste des poissons/insectes avec quelques données

--- JSCode/index.js
+++ JSCode/index.js
@@ -1,14 +1,35 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var insectesN = require('./insectesN.json');
+var poissonsN = require('./poissonsN.json');
+var listeCommandes = 'test';
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(insectesN);
  });
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
+ if (msg.content === '!insectes nord') {
+   msg.reply('voici la liste des insectes disponibles actuellement : \n' + listeAnimaux(insectesN));
+ } else if (msg.content === '!poissons nord') {
+   msg.reply('voici la liste des poissons disponibles actuellement : \n' + listeAnimaux(poissonsN));
+ } else if (msg.content === '!aide') {
+    msg.reply(listeCommandes);
  }
- });
+});
 
 client.login('NjkzODI5NjE3NDIwNTk5MzM4.XoCxhA.PeMxCsTF7wldYrTPrQ75VzanSY4');
+
+function listeAnimaux(liste) {
+  var animaux = "";
+  for (i = 0; i < liste.length; i++) {
+    animaux += "Nom: " + liste[i].nom + "\n";
+    animaux += "Période: " + liste[i].période + "\n";
+    animaux += "Heure: " + liste[i].heure + "\n";
+    animaux += "Lieu: " + liste[i].lieu + "\n";
+    animaux += "Prix: " + liste[i].prix + " clochettes \n";
+    animaux += "\n";
+  }
+  return animaux;
+}
