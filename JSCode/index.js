8c274590e1734088ba0e0ca9e9be56c495c41216
Simon Ribaut <simon.ribaut@outlook.com>
2020-03-30 02:44:31 +0200

    chercher insecte, poisson, fix bugs

--- JSCode/index.js
+++ JSCode/index.js
@@ -2,36 +2,54 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var insectesN = require('./insectesN.json');
 var poissonsN = require('./poissonsN.json');
-var listeCommandes = '!insectes nord\n!poissons nord';
-const nomMois = ["Janvier", "Février" ,"Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+var listeCommandes = '!insectes nord\n!poissons nord\n!info';
+const nomMois = ["Janvier", "Février" ,"Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
- if (msg.content === '!insectes nord') {
-   msg.reply('voici la liste des insectes disponibles actuellement : \n' + listeAnimauxNow(insectesN));
- } else if (msg.content === '!poissons nord') {
-   msg.reply('voici la liste des poissons disponibles actuellement : \n' + listeAnimauxNow(poissonsN));
- } else if (msg.content === '!aide') {
+  console.log(msg.content);
+  if (msg.content === '!insectes nord') {
+     msg.reply('voici la liste des insectes disponibles actuellement : \n' + listeInsectesNow(insectesN));
+  } else if (msg.content === '!poissons nord') {
+     msg.reply('voici la liste des poissons disponibles actuellement : \n' + listePoissonsNow(poissonsN));
+  } else if (msg.content.split(' ')[0] === '!details') {
+    // Garder seulement le nom de l'animal, apres l'espace
+    nomAnimal = msg.content.slice(9 , msg.content.length);
+    infoAnimal = findAnimal(nomAnimal);
+    console.log(infoAnimal);
+    if (infoAnimal != null) {
+      msg.reply("voici le détail de l'animal : \n" + infoAnimal);
+    } else {
+      msg.reply("ton animal n'a pas été trouvé.");
+    }
+  } else if (msg.content === '!aide') {
     msg.reply('voici la liste des commandes disponibles : \n' + listeCommandes);
- }
+  }
 });
 
 client.login('NjkzODI5NjE3NDIwNTk5MzM4.XoCxhA.PeMxCsTF7wldYrTPrQ75VzanSY4');
 
-function listeAnimauxNow(liste) {
+function listeInsectesNow(liste) {
   var animaux = "";
   for (i = 0; i < liste.length; i++) {
     if (liste[i].période === "Toute l'année" || isActualM(liste[i].période)) {
       if (isActualH(liste[i].heure)) {
-        animaux += "Nom: " + liste[i].nom + "\n";
-        animaux += "Période: " + liste[i].période + "\n";
-        animaux += "Heure: " + liste[i].heure + "\n";
-        animaux += "Lieu: " + liste[i].lieu + "\n";
-        animaux += "Prix: " + liste[i].prix + " clochettes \n";
-        animaux += "\n";
+        animaux += afficheInsecte(liste[i]);
+      }
+    }
+  }
+  return animaux;
+}
+
+function listePoissonsNow(liste) {
+  var animaux = "";
+  for (i = 0; i < liste.length; i++) {
+    if (liste[i].période === "Toute l'année" || isActualM(liste[i].période)) {
+      if (isActualH(liste[i].heure)) {
+        animaux += affichePoisson(liste[i]);
       }
     }
   }
@@ -71,4 +89,42 @@ function isActualH(heure) {
         return debutH > actuelH && actuelH < finH;
       }
     }
+}
+
+function findAnimal(nomAnimal) {
+  var found = false;
+  for (i = 0; !found && i < insectesN.length; i++) {
+    if (!found && insectesN[i].nom === nomAnimal) {
+      found = true;
+      return afficheInsecte(insectesN[i]);
+    }
+    if (!found && poissonsN[i].nom === nomAnimal) {
+      found = true;
+      return affichePoisson(poissonsN[i]);
+    }
+  }
+  return null;
+}
+
+function afficheInsecte(animalJSON) {
+  var animal = "";
+  animal += "Nom: " + animalJSON.nom + "\n";
+  animal += "Période: " + animalJSON.période + "\n";
+  animal += "Heure: " + animalJSON.heure + "\n";
+  animal += "Lieu: " + animalJSON.lieu + "\n";
+  animal += "Prix: " + animalJSON.prix + " clochettes \n";
+  animal += "\n";
+  return animal;
+}
+
+function affichePoisson(animalJSON) {
+  var animal = "";
+  animal += "Nom: " + animalJSON.nom + "\n";
+  animal += "Période: " + animalJSON.période + "\n";
+  animal += "Heure: " + animalJSON.heure + "\n";
+  animal += "Lieu: " + animalJSON.lieu + "\n";
+  animal += "Prix: " + animalJSON.prix + " clochettes \n";
+  animal += "Taille: " + animalJSON.taille + "\n";
+  animal += "\n";
+  return animal;
 }
