58a052ef32871097ce201e01bbf4deed2edd1270
neon <wafigrissi@gmail.com>
2020-03-30 02:13:24 +0200

    maj type listeCommandes et check sur l_hemisphere

--- JSCode/index.js
+++ JSCode/index.js
@@ -1,74 +1,105 @@
+// set up
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var insectesN = require('./insectesN.json');
 var poissonsN = require('./poissonsN.json');
-var listeCommandes = '!insectes nord\n!poissons nord';
-const nomMois = ["Janvier", "Février" ,"Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+//TODO: traiter, eventuellement, plus de filtres (sans ordre)
+var listeCommandes = ["!insectes nord","!poissons nord","!aide"];
+const nomMois = ["Janvier", "Février" ,"Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
+// log le tag du bot quand il est pret
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
- });
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
+// Traitement des commandes
 client.on('message', msg => {
- if (msg.content === '!insectes nord') {
-   msg.reply('voici la liste des insectes disponibles actuellement : \n' + listeAnimauxNow(insectesN));
- } else if (msg.content === '!poissons nord') {
-   msg.reply('voici la liste des poissons disponibles actuellement : \n' + listeAnimauxNow(poissonsN));
- } else if (msg.content === '!aide') {
-    msg.reply('voici la liste des commandes disponibles : \n' + listeCommandes);
+    // Vérifier le premier input de la commande
+    if(msg.content.split(' ')[0]==='!insectes' || msg.content.split(' ')[0]==='!poissons')
+    { // Si c'est une commande concernant les insectes ou les poissons
+        if(msg.content.split(' ')[1]!='nord' && msg.content.split(' ')[1]!='sud')
+        { // si le premier parametre n'est pas l'hemisphere
+            msg.reply("oups, tu as oublié l'hémisphère, saisis 'sud' ou 'nord'.");
+        }
+        else
+        { // si le premier parametre est l'hemisphere
+            if (msg.content.split(' ')[1]!='nord')
+            { // si l'hemisphere est le nord
+                msg.reply('voici la liste des insectes disponibles actuellement : \n' + listeAnimauxNow(insectesN));
+            } else if (msg.content.split(' ')[1]!='sud')
+            { // si l'hemisphere est le sud
+                msg.reply('voici la liste des poissons disponibles actuellement : \n' + listeAnimauxNow(poissonsN));
+            }
+     }
+ } else if (msg.content === '!aide')
+ { // si c'est une commande pour avoir de l'aide
+        msg.reply('voici la liste des commandes disponibles : \n' + commandesToString());
  }
 });
 
-client.login('NjkzODI5NjE3NDIwNTk5MzM4.XoCxhA.PeMxCsTF7wldYrTPrQ75VzanSY4');
+// Token du bot
+client.login('NjkzOTU0MjUxMzQ4NTA4NzUy.XoElTQ.XiXC-gm3DefG3BloLv7OiWROI-E');
 
 function listeAnimauxNow(liste) {
-  var animaux = "";
-  for (i = 0; i < liste.length; i++) {
-    if (liste[i].période === "Toute l'année" || isActualM(liste[i].période)) {
-      if (isActualH(liste[i].heure)) {
-        animaux += "Nom: " + liste[i].nom + "\n";
-        animaux += "Période: " + liste[i].période + "\n";
-        animaux += "Heure: " + liste[i].heure + "\n";
-        animaux += "Lieu: " + liste[i].lieu + "\n";
-        animaux += "Prix: " + liste[i].prix + " clochettes \n";
-        animaux += "\n";
-      }
+    var animaux = "";
+    for (i = 0; i < liste.length; i++) {
+        if (liste[i].période === "Toute l'année" || isActualM(liste[i].période)) {
+            if (isActualH(liste[i].heure)) {
+                animaux += "Nom: " + liste[i].nom + "\n";
+                animaux += "Période: " + liste[i].période + "\n";
+                animaux += "Heure: " + liste[i].heure + "\n";
+                animaux += "Lieu: " + liste[i].lieu + "\n";
+                animaux += "Prix: " + liste[i].prix + " clochettes \n";
+                animaux += "\n";
+            }
+        }
     }
-  }
   return animaux;
 }
 
 function monthToInt(mois) {
-  return nomMois.indexOf(mois);
+    return nomMois.indexOf(mois);
 }
 
 function isActualM(periode) {
-  var debutM = periode.split(' - ')[0];
-  var finM = periode.split(' - ')[1];
-  debutM = monthToInt(debutM);
-  finM = monthToInt(finM);
-  var date = new Date();
-  var actuelM = date.getMonth();
-  return debutM <= actuelM && actuelM <= finM;
+      var debutM = periode.split(' - ')[0];
+      var finM = periode.split(' - ')[1];
+      debutM = monthToInt(debutM);
+      finM = monthToInt(finM);
+      var date = new Date();
+      var actuelM = date.getMonth();
+      return debutM <= actuelM && actuelM <= finM;
 }
 
 function isActualH(heure) {
-    var debutH = heure.split(' - ')[0];
-    debutH = debutH.slice(0, -1);
-    var finH = heure.split(' - ')[1];
-    finH = finH.slice(0, -1);
-    var date = new Date();
-    var actuelH = date.getHours();
-    debutH = parseInt(debutH);
-    finH = parseInt(finH);
-    actuelH = parseInt(actuelH);
-    if (debutH < finH) {
-      return debutH <= actuelH && actuelH < finH;
-    } else {
-      if (debutH <= actuelH && actuelH <= 23) {
-        return debutH <= actuelH && actuelH > finH;
-      } else {
-        return debutH > actuelH && actuelH < finH;
-      }
+        var debutH = heure.split(' - ')[0];
+        debutH = debutH.slice(0, -1);
+        var finH = heure.split(' - ')[1];
+        finH = finH.slice(0, -1);
+        var date = new Date();
+        var actuelH = date.getHours();
+        debutH = parseInt(debutH);
+        finH = parseInt(finH);
+        actuelH = parseInt(actuelH);
+        if (debutH < finH) {
+            return debutH <= actuelH && actuelH < finH;
+        } else {
+              if (debutH <= actuelH && actuelH <= 23) {
+                    return debutH <= actuelH && actuelH > finH;
+              } else {
+                    return debutH > actuelH && actuelH < finH;
+              }
+        }
+}
+
+// lister les commandes
+function commandesToString() {
+    var res = "";
+
+    for(i=0; i<listeCommandes.length; i++)
+    {
+        res += listeCommandes[i];
     }
+
+    return res;
 }
